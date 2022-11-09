9b24585959bab39d94614f052a35ab5e34f218fd
dauqu <sujeetsingh734321@gmail.com>
2022-11-09 14:20:01 +0530

    new push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import  axios  from "axios";
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Slider from "./components/Slider";
+// allow axios credentials
+
+axios.defaults.withCredentials = true;
 function App() {
   return (
     <>
