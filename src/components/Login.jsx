3b202e165d9207dc90f9d746fd2c6e3d03bef92d
dauqu <sujeetsingh734321@gmail.com>
2022-11-09 14:39:09 +0530

    new push axios

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import SecondHeader from "./SecondHeader";
 import axios from "axios";
+axios.defaults.withCredentials = true;
 
 function Login() {
   const [email, setEmail] = useState("");
